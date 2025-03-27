636d429e62ae2dd897190959ab74cd5909676dd4
Manish Kc <manishkc2004@gmail.com>
2025-03-27 21:21:56 +0545

    Initial Commit

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.product, name='product'),
-    path('/productdetail', views.productDetail, name='productDetail')
+    path('/productdetail', views.productDetail, name='productDetail'),
+    
 ]
