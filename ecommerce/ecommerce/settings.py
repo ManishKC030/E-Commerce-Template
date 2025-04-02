2869456b6b4c2fa393aa362feaaa1e73139cde65
Manish Kc <manishkc2004@gmail.com>
2025-04-02 20:51:41 +0545

    Initial COmmit

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-jf=02$jo4h_8+vpa-e9^2b^n7m)tw2%b-@gxi!y%&(#qoxuet_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
