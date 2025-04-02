1c66a5f3f4797d28af3355c98ed483d259aeef4c
Manish Kc <manishkc2004@gmail.com>
2025-04-02 20:27:49 +0545

    Initial Commit

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,4 +19,5 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.name
