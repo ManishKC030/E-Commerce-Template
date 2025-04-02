94b4b01afd1c02883628852632c795cea6a0db66
Manish Kc <manishkc2004@gmail.com>
2025-04-02 20:18:59 +0545

    Initial Commit

--- ecommerce/users/models.py
+++ ecommerce/users/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
 
