87b1102954bf2b86facd4e18f12a9601e6a33d24
Manish Kc <manishkc2004@gmail.com>
2025-04-06 18:11:32 +0545

    initial Commit

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -16,7 +16,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    image = models.ImageField(upload_to='uploads/')
+    image1 = models.ImageField(upload_to='uploads/')
+    image2 = models.ImageField(upload_to='uploads/')
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
